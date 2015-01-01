2bf96b722fe8225f7eca125488e422dc906683a5
Barbie <barbie@missbarbell.co.uk>
2015-01-01 16:09:50 +0000

    updated copyright year

--- lib/Template/Plugin/Lingua/EN/Inflect.pm
+++ lib/Template/Plugin/Lingua/EN/Inflect.pm
@@ -281,7 +281,7 @@ Lingua::EN::Inflect module, which does all the clever stuff.
 =head1 COPYRIGHT & LICENSE
 
 Copyright (C) 2005-2014 Andrew Ford
-Copyright (C) 2014      Barbie for Miss Barbell Productions.
+Copyright (C) 2014-2015 Barbie for Miss Barbell Productions.
 
 This distribution is free software; you can redistribute it and/or
 modify it under the Artistic Licence v2.
